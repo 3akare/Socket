1de7aaa0ef5d03a0b8e80c54e0ad230007d96306
David Bakare <bakaredavid007@gmail.com>
2023-08-04 02:26:17 +0100

    vite config

---
+++ client/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+  // Other Vite configuration settings...
+  build: {
+    // Set the publicPath to "http://localhost/"
+    base: "http://localhost/",
+  },
+};
