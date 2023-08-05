093508ca10b32493758c3617ededc7d80f6bc291
David Bakare <bakaredavid007@gmail.com>
2023-08-05 13:01:53 +0100

    another fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const io = require("socket.io")(3000, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:4173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "http://localhost:3000",
+      "http://88.80.187.75",
+    ],
   },
 });
 
