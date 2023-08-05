dd57a2637f2cd89c857d39ce3dab02cdc2d7442d
David Bakare <bakaredavid007@gmail.com>
2023-08-05 13:39:21 +0100

    switch to web sockets

--- client/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-export default {
-  // Other Vite configuration settings...
-  build: {
-    // Set the publicPath to "http://localhost/"
-    base: "http://localhost/",
-  },
-};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
-const io = require("socket.io")(3000, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      "http://localhost:3000",
-      "http://88.80.187.75:4173",
-      "http://88.80.187.75:5173",
-    ],
-  },
+const http = require("http");
+const WebSocket = require("ws");
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+// WebSocket connection handling
+wss.on("connection", (ws) => {
+  console.log("A new client connected.");
+
+  // Handle incoming messages from clients
+  ws.on("message", (message) => {
+    console.log("Received message:", message);
+    // Broadcast the message to all connected clients (including the sender)
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle client disconnection
+  ws.on("close", () => {
+    console.log("Client disconnected.");
+  });
 });
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`WebSocket server started on port ${port}`);
 });
