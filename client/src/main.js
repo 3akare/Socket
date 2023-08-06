fbd3b0292a8a28c82d7898e6cbdc67bc75d67cc2
David Bakare <bakaredavid007@gmail.com>
2023-08-06 14:03:52 +0100

    frontend initial build

---
+++ client/src/main.js
@@ -0,0 +1,35 @@
+const socket = new WebSocket("ws://88.80.187.75:3000");
+
+socket.onopen = () => {
+  console.log("WebSocket connection opened.");
+};
+
+socket.onmessage = (event) => {
+  const messagesList = document.getElementById("messages");
+  const messageItem = document.createElement("li");
+
+  const reader = new FileReader();
+  reader.onload = () => {
+    messageItem.textContent = reader.result; // reader.result contains the text data
+    messagesList.appendChild(messageItem);
+  };
+  reader.readAsText(event.data); // Read the Blob as text
+};
+
+socket.onclose = () => {
+  console.log("WebSocket connection closed.");
+};
+
+function sendMessage() {
+  const messageInput = document.getElementById("messageInput");
+  const message = messageInput.value;
+  socket.send(message);
+  messageInput.value = "";
+}
+
+document.querySelector('textarea').addEventListener('keydown', (event)=>{
+  if (event.code === 'Enter'){
+    event.preventDefault()
+    sendMessage()
+  }
+})
